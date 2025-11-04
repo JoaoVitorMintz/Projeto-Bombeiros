abe1c2e0ed66b74e64f97ca0b318a950341fa414
João Vitor G A Mintz <joaomintzz@gmail.com>
2025-11-04 20:08:32 -0300

    Continuação do projeto (supostamente só falta o algoritmo de dijkstra)

--- main.c
+++ main.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX_ARESTAS 200
 
 // Função que ordena esquinas do menor tempo para o maior tempo.
-int reordenar_sequencia(int m, int *M) {
+void reordenar_sequencia(int origem[], int destino[], int tempo[], int qtdArestas) {
+    for (int i = 0; i < qtdArestas - 1; i++) {
+        for (int j = i+1; j < qtdArestas; j++) {
+            if (tempo[i] > tempo[j]) {
+                // Reordenar para começar com o menor valor de tempo no vetor
+                int temp = tempo[i];
+                tempo[i] = tempo[j];
+                tempo[j] = temp;
 
+                // Reordenar origem para manter a sequência com base no tempo
+                temp = origem[i];
+                origem[i] = origem[j];
+                origem[j] = temp;
+
+                // Reordenar destino para manter a sequência com base no tempo
+                temp = destino[i];
+                destino[i] = destino[j];
+                destino[j] = temp;
+            }
+        }
+    }
 }
 
 // Função que cria um vetor com o menor tempo percorrido.
-void rota_mais_rapida_dijkstra(int *M, int n, int m) {
-    int E[m]; // Conjunto E que armazena as esquinas do mapa M
-    int T[50]; // Vetor para armazenar tempos
-    int R[50]; // Vetor para armazenar rotas mais rápidas
-    for (int i = 0; i < E[i]; i++) {
-        T[i] = 0;
-    }
-    T[1] = 0;
-    while (E[m] != NULL) {
-        int v = E[m--];
-    }
+void rota_mais_rapida_dijkstra(int origem[], int destino[], int tempo[], int n, int m, int qtdArestas) {
+    reordenar_sequencia(origem, destino, tempo, qtdArestas);
 
 }
 
@@ -26,6 +39,11 @@ int main(void) {
     int m; // número de esquinas que constam no mapa.
     FILE *entrada; // Variável para leitura do bombeiro.txt.
     int linha[100];
+    int origem[MAX_ARESTAS]; // Vetor que armazena o ponto de origem
+    int destinos[MAX_ARESTAS]; // Vetor que armazena o ponto de chegada
+    int tempo[MAX_ARESTAS]; // Vetor que armazena o tempo entre os dois pontos
+    int a, b, t;
+    int qtdArestas = 0;
 
     entrada = fopen("bombeiro.txt", "r");
 
@@ -44,9 +62,21 @@ int main(void) {
         printf("%s", linha);
     }
     printf("\nCom base nisso, esta é a sequência mais curta: ");
-    
+
+    while(1) {
+        fscanf(entrada, "%d", &a);
+        if (a == 0) break; // Fim do arquivo
+        fscanf(entrada, "%d %d", &b, &t);
+
+        origem[qtdArestas] = a;
+        destinos[qtdArestas] = b;
+        tempo[qtdArestas] = t;
+        qtdArestas++;
+    }
 
     fclose(entrada);
+
+    rota_mais_rapida_dijkstra(origem, destinos, tempo, n, m, qtdArestas);
 
     return 0;
 }
